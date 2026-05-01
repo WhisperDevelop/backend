03212efe9396611bea23f5cd2c0edc464e0204d3
KimThuong <nakamurakimthuong@gmail.com>
2026-05-01 11:11:54 +0900

    Create UserSeeder

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -2,16 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * データベースにユーザーデータを投入する。
      */
     public function run(): void
     {
-        //
+        /*
+        |--------------------------------------------------------------------------
+        | テスト用ユーザー（固定）
+        |--------------------------------------------------------------------------
+        |
+        | APIテスト用として、ログインしやすいユーザーを用意する。
+        |
+        */
+
+        User::create([
+            'name' => 'Admin01',
+            'email' => 'test@test.com',
+            'password' => Hash::make('W@admin01'),
+        ]);
+
+        User::create([
+            'name' => 'Admin02',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('W@admin02'),
+        ]);
+
     }
 }
